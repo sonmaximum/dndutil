45a02ea23d7800c26f0769f9c6aa343944ee0018
Max Juchheim <maxjuchheim@gmail.com>
2017-08-08 23:49:11 -0400

    Add Character model

--- dndpimp/admin.py
+++ dndpimp/admin.py
@@ -1,12 +1,20 @@
 from django.contrib import admin
 
 # Register your models here.
-from dndpimp.models import Item
+from dndpimp.models import *
 
 class ItemAdmin(admin.ModelAdmin):
 	model = Item
 	list_display = ('name', 'description',)
 	prepopulated_fields = {'slug': ('name',)}
 		
-		
 admin.site.register(Item, ItemAdmin)
+
+
+class CharacterAdmin(admin.ModelAdmin):
+	model = Character
+	list_display = ('name', 'description', 'user')
+	prepopulated_fields = {'slug': ('name',)}
+
+
+admin.site.register(Character, CharacterAdmin)
--- dndpimp/models.py
+++ dndpimp/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
@@ -5,3 +6,9 @@ class Item(models.Model):
 	name = models.CharField(max_length=255)
 	description = models.TextField()
 	slug = models.SlugField(unique=True)
+
+class Character(models.Model):
+	name = models.CharField(max_length=255)
+	description = models.TextField()
+	slug = models.SlugField(unique=True)
+	user = models.OneToOneField(User, blank=True, null=True)
