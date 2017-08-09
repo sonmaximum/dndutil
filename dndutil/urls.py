239f688d6fe9bf7ec0c53c56cc4ecc8e962ea5ab
Max Juchheim <maxjuchheim@gmail.com>
2017-08-08 22:52:01 -0400

    add password function

--- dndutil/urls.py
+++ dndutil/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from dndpimp import views as dndpimp_views
 from django.views.generic import TemplateView
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
 	url(r'^$', dndpimp_views.index, name='home'),
@@ -33,6 +34,21 @@ urlpatterns = [
 	url(r'^items/(?P<slug>[-\w]+)/edit/$',
 		dndpimp_views.edit_item,
 		name='edit_item'),
+	url(r'^accounts/password/reset/$', auth_views.password_reset,
+		{'template_name': 'registration/password_reset_form.html'},
+		name='password_reset'),
+	url(r'^accounts/password/reset/done/$', 
+		auth_views.password_reset_done,
+		{'template_name': 'registration/password_reset_done.html'},
+		name='password_reset_done'),
+	url(r'^accounts/password/reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1-20})/$',
+		auth_views.password_reset_confirm,
+		{'template_name': 'registration/password_reset_confirm.html'},
+		name='password_reset_confirm'),
+	url(r'^accounts/password/done/$', 
+		auth_views.password_reset_complete,
+		{'template_name': 'registration/password_reset_complete.html'},
+		name='password_reset_complete'),
 	url(r'^accounts/', include('registration.backends.simple.urls')),
     url(r'^admin/', admin.site.urls),
 ]
