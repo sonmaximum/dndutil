dae32a1c78ce815e46212d9ae5385a684a8f2612
Max Juchheim <maxjuchheim@gmail.com>
2017-08-29 23:49:29 -0400

    Added production settings file.

--- dndutil/wsgi.py
+++ dndutil/wsgi.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dndutil.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dndutil.settings_production")
 
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
