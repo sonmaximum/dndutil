a16c5f84c1a834f4f10a56f12cceaefd14feb668
sonmaximum <maxjuchheim@gmail.com>
2018-05-02 01:54:53 -0400

    fix linter error

--- dndpimp/models.py
+++ dndpimp/models.py
@@ -1,13 +1,16 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
 class Item(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     slug = models.SlugField(unique=True)
+
     def __str__(self):
         return self.name
+
 
 class Party(models.Model):
     class Meta:
@@ -15,14 +18,21 @@ class Party(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     slug = models.SlugField(unique=True)
+
     def __str__(self):
         return self.name
+
 
 class Character(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     slug = models.SlugField(unique=True)
-    user = models.ForeignKey(User, blank=True, null=True, on_delete=models.CASCADE)
-    party = models.ForeignKey(Party, blank=True, null=True, on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        User, blank=True, null=True, on_delete=models.CASCADE
+        )
+    party = models.ForeignKey(
+        Party, blank=True, null=True, on_delete=models.CASCADE
+        )
+
     def __str__(self):
         return self.name
